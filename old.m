18d619f412d2128032d3bbb1bb38dec8dcb5920f
Cristian Chitiva <cychitivav@unal.edu.co>
2022-01-27 12:14:47 -0500

    Commands xpc added to old.m

--- old.m
+++ old.m
@@ -26,4 +26,18 @@ xpcgetCC('installed')
 slrtExplorer
 
  xpcexplr
+
+ %% Imporrtant commands 
+ % stablish  connection  with xpc target
+ tg = xpctarget.xpc('TCPIP','192.168.1.12','22222')
+ 
+tg.targetping
+tg.Application
+
+tg.load('Code2XPCtarget\GUI_User_V1_3')
+%%
+tg.StateLog
+tg.Status
+tg.StopTime
+tg.start
  
