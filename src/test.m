9622b1de9fc264af5007e2adb0e7946cf6fbbc36
Cristian Chitiva <cychitivav@unal.edu.co>
2022-03-24 14:20:16 -0500

    [UPDATE] test models * [UPDATE] GUI_V2 build * [FIX] git ignore mex files * [RENAME] test models folder

--- src/test.m
+++ src/test.m
@@ -2,28 +2,31 @@ clear, clc, close all
 %% Set up
 % If deault is configured  
 tg = xpctarget.xpc;
-modelName = 'motionControlModel';
+modelName = 'testModels/GUI_V2';
+tg.unload()
  
 connectivity = tg.targetping;           % check conectivity
 if strcmp(connectivity,'success')
-    %rtwbuild(modelName);    
+    rtwbuild(modelName);    
     tg.load(modelName);
     %open_system(modelName);
     tg.stop;
     tg.ShowSignals ='off';
-%     tg.ShowParameters ='off';
+    tg.ShowParameters ='off';
 else
-    disp('Failed connection');
+    error('Failed connection');
 end
 
 % create scopes
 % 'target' 'host' 'file'
-sc1 = addscope(tg,'target',1)
-sc2 = addscope(tg,'target',2)
-sc3 = addscope(tg,'target',3)
-sc4 = addscope(tg,'target',4)
+sc1 = addscope(tg,'target',1);
+sc2 = addscope(tg,'target',2);
+sc3 = addscope(tg,'target',3);
+sc4 = addscope(tg,'target',4);
+
+sc5 = addscope(tg,'target',5);
 %% Input signal id
-id_input=[];
+id_input = zeros(12,1);
 
 for k=1:12
     name = ['MM-16-AT Analog Input/p',num2str(k)];
@@ -32,38 +35,29 @@ end
 
 id_distance= id_input(1:2:11);   
 id_currents = id_input(2:2:12);
-
 %% PWM signal id 
-id_PWM=[];
+id_PWM = zeros(6,1);
 
 for k=1:6
-    %name = ['Motors - F4 STM - PWM/If Action Subsystem6/Referece/From File1/s',num2str(k)]
-    name = ['Multiport Switch/s',num2str(k)]      
-    id_PWM(k)=getsignalid(tg,name);
+    name = ['Multiport Switch/s',num2str(k)];    
+    id_PWM(k) = getsignalid(tg,name);
 end
-
-
 %% Read signals into scopes 
-sc1=tg.getscope(1)
-sc1.set('Grid', 'on')
-sc1.addsignal(id_distance)
-sc1.YLimit = [0 5];
+sc1.addsignal (id_distance);
+sc1.DisplayMode = 'Numerical';
 sc1.start;
 
-sc2 = tg.getscope(2);
-sc2.addsignal (id_distance);
-sc2.DisplayMode = 'Numerical';
+sc2.set('Grid', 'on')
+sc2.addsignal(id_currents)
+sc2.YLimit = [0 5];
 sc2.start;
 
-sc3=tg.getscope(3)
-sc3.set('Grid', 'on')
-sc3.addsignal(id_currents)
-sc3.YLimit = [0 5];
+sc3.addsignal(id_PWM);
+%sc4.DisplayMode = 'Numerical';
 sc3.start;
 
-sc4 = tg.getscope(4);
 sc4.addsignal(id_PWM);
-%sc4.DisplayMode = 'Numerical';
+sc4.DisplayMode = 'Numerical';
 sc4.start;
 
 Constant1 = 0;
@@ -78,10 +72,13 @@ id_ki = tg.getparamid('Discrete PID Controller/Integral Gain','Gain');
 
 id_ref = tg.getparamid('Reference','Value');
 
-
+%% Spy
+%xpctargetspy
 %% Run movements
-X=0,        Y=0,        Z=0.9;
-Roll=-0,  Pitch=-0,   Yaw=0;
+tg.start;
+input('Empezar?')
+X=0;        Y=0;        Z=0.75;
+Roll=0;  Pitch=-0;   Yaw=0;
 
 % measured distance
 id_dist_m = tg.getsignalidsfromlabel('dist_input');
@@ -96,12 +93,20 @@ input('Presione Enter para terminar')
 tg.setparam(id_mode,0);      % stop movement 
 
 %% log
-saveData=false;
+% saveData=false;
+% 
+% if saveData
+%     
+% prefix=datestr( now ,'mm_dd_HH_MM_');
+% title='data.mat';
+% fileName=['./dumpOutput/', prefix , title]
+% 
+% end
 
-if saveData
-    
-prefix=datestr( now ,'mm_dd_HH_MM_');
-title='data.mat';
-fileName=['./dumpOutput/', prefix , title]
-
-end
+tg.stop;
+data = tg.OutputLog(:,2:7)-50;
+% Shape
+% s%c%c%c%c%c%c
+%%
+plot(data)
+legend('1','2','3','4','5','6')
