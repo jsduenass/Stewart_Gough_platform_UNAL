fde1aa4fdc75e933223e8c334cef2768e0a44cbf
Sebastian <jsduenass@unal.edu.co>
2022-03-29 17:02:40 -0500

    FREEDOS OS Use rufus to set up the flash drive as a bootable OS and config build_PCM_OS to make IP compatible * [ADD] OS in bin folder * Change default from PCM2 to PCM1 * config PCM1 in build_PCM_OS * Change default IP to 192.168.0.12

--- src/build_PCM_OS.m
+++ src/build_PCM_OS.m
@@ -1,7 +1,14 @@
 % Build PCM Operating System 
 
-tgs=xpctarget.targets
-env= tgs.Item('TargetPC2')
+tgs = xpctarget.targets;
+tgs.makeDefault('TargetPC1');
+env= tgs.Item('TargetPC1')
+env.TcpIpTargetAddress = '192.168.0.12';
+env.TcpIpSubNetMask = '255.255.255.0';
+env.TcpIpTargetPort='22222'
+
+env.TargetBoot='StandAlone'
+
 env.TargetBoot='StandAlone'
 env.TargetBoot='DOSLoader'
 
